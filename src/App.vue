960d8367e57d4411277243673c294b7fe2245c64
Vanessa_Riquelme <vriquelmefe@gmail.com>
2021-07-24 00:22:10 -0400

    subiendo proyecto

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h2> Crea una nueva tarea</h2>
+    <label for="tarea" ><b>Tarea </b></label>
+    <input type="text" v-model="nuevoIngreso" placeholder="Ingresa nueva tarea">
+    <button @click="agregarTarea">Crear</button>
+    <h4>Lista</h4>
+    <ul >
+    <ListaTarea v-for="(tarea , index) in tareas" :key="index" :tareaNueva="tarea" v-on:eliminarTarea="tareas.splice(index,1)"></ListaTarea>
+    </ul>    
+  </div>
+</template>
+
+<script>
+import ListaTarea from '@/components/ListaTarea.vue';
+ 
+
+export default {
+  name: "App",
+  components: {
+    ListaTarea
+  },
+  data: () => ({
+    nuevoIngreso : '',
+    tareas: []
+  }),
+  methods: {
+    agregarTarea() {
+      this.tareas.push( this.nuevoIngreso )
+      this.nuevoIngreso = ''
+    },
+  }
+   
+  
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+ul{
+    margin: 0 auto;
+    text-align: left;
+    padding-left: 15% ;
+    width: 20%;
+  }
+</style>
---
+++ src/components/ListaTarea.vue
@@ -0,0 +1,24 @@
+import Formulario from '@/components/Formulario.vue';
+<template>
+  <div >
+      <li >{{ tareaNueva }} <button v-on:click="$emit('eliminarTarea')">Eliminar</button></li> 
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListaTarea',
+  
+ 
+  props: {
+    tareaNueva: {
+      type: String,
+    },
+  },
+  methods: {
+  }
+  
+}
+
+</script>
